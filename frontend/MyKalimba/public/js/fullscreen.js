b2119f6963146de00f44d85d951294bb0435a764
NguyenVuKhanh <nguyenvukhanh392@gmail.com>
2026-01-25 23:47:52 +0700

    copy js pick color into public folder

---
+++ frontend/MyKalimba/public/js/fullscreen.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  // Кнопка переключения Фуллскрина
+  const fullscreenButton = $("#fullscreenButton");
+  // Блок, который необходимо делать в фулл скрин
+  const mainContainer = $("#main-container")[0];
+
+  // Событие на нажатие на кнопку Фуллскрина
+  fullscreenButton.on("click", function () {
+    if (document.fullscreenElement) {
+      exitFullscreen();
+    } else {
+      enterFullscreen();
+    }
+  });
+
+  // Событие вызывается при смене состояния фуллскрина
+  $(document).on("fullscreenchange", function () {
+    // Если полный экран выключен, выходим из него
+    if (!document.fullscreenElement) {
+      $("#fullscreen-on").show();
+      $("#fullscreen-off").hide();
+      $("#main-container").removeClass("fullscreen");
+    }
+  });
+
+  // Функция входа в полный экран
+  function enterFullscreen() {
+    if (mainContainer.requestFullscreen) {
+      mainContainer.requestFullscreen();
+    } else if (mainContainer.mozRequestFullScreen) {
+      mainContainer.mozRequestFullScreen();
+    } else if (mainContainer.webkitRequestFullscreen) {
+      mainContainer.webkitRequestFullscreen();
+    } else if (mainContainer.msRequestFullscreen) {
+      mainContainer.msRequestFullscreen();
+    }
+    $("#fullscreen-on").hide();
+    $("#fullscreen-off").show();
+    $("#main-container").addClass("fullscreen");
+  }
+
+  // Функция выхода из полного экрана
+  function exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    $("#fullscreen-on").show();
+    $("#fullscreen-off").hide();
+    $("#main-container").removeClass("fullscreen");
+  }
+});
---
+++ frontend/MyKalimba/public/js/pico-theme-switcher.js
@@ -0,0 +1,63 @@
+/* Код кнопки смены полностью взят с оф. сайта */
+"use strict";
+!(function () {
+  var e = {
+    _scheme: "auto",
+    buttonsTarget: ".theme-switcher",
+    localStorageKey: "picoPreferredColorScheme",
+    init() {
+      this.scheme = this.schemeFromLocalStorage;
+      this.initSwitchers();
+    },
+    get schemeFromLocalStorage() {
+      return window.localStorage &&
+        null !== window.localStorage.getItem(this.localStorageKey)
+        ? window.localStorage.getItem(this.localStorageKey)
+        : this._scheme;
+    },
+    get preferredColorScheme() {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+    },
+    initSwitchers() {
+      $(this.buttonsTarget).on("click", function () {
+        e.scheme = e.scheme === "dark" ? "light" : "dark";
+      });
+    },
+    addButton(config) {
+      $(config.tag, {
+        class: config.class,
+        text: config.text,
+      }).appendTo(config.target);
+    },
+    set scheme(e) {
+      "auto" == e
+        ? "dark" == this.preferredColorScheme
+          ? (this._scheme = "dark")
+          : (this._scheme = "light")
+        : ("dark" != e && "light" != e) || (this._scheme = e);
+      this.applyScheme();
+      this.schemeToLocalStorage();
+    },
+    get scheme() {
+      return this._scheme;
+    },
+    applyScheme() {
+      $("html").attr("data-theme", this.scheme);
+      let darkmode = e.scheme === "dark";
+      if (darkmode) {
+        $("[theme=light]", this.buttonsTarget).show();
+        $("[theme=dark]", this.buttonsTarget).hide();
+      } else {
+        $("[theme=light]", this.buttonsTarget).hide();
+        $("[theme=dark]", this.buttonsTarget).show();
+      }
+    },
+    schemeToLocalStorage() {
+      window.localStorage &&
+        window.localStorage.setItem(this.localStorageKey, this.scheme);
+    },
+  };
+  e.init();
+})();
